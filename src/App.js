8eac523ed5f73a68cb5024bd9a79bc564d5f2c27
k1000oEnz <enriquezcamilofranco@gmail.com>
2026-02-20 18:57:03 -0300

    App.js - Funcionalidad de los fetch y base del front. .gitignore - Agregado archivo .env

--- src/App.js
+++ src/App.js
@@ -13,16 +13,48 @@ function App() {
 
     setLoading(true);
     try {
+
+      //Nota: Guardo el url en un .env por qué es como yo lo haría en un proyecto real, para no exponer el url en el código.
+      const candidateUrl = process.env.REACT_APP_BASE_URL + `/api/candidate/get-by-email?email=${email}`;
+      const candidateResponse = await fetch(candidateUrl);
+      const candidateData = await candidateResponse.json();
+      console.log(candidateData);
+      setCandidate(candidateData);
+
+      const positionsUrl = process.env.REACT_APP_BASE_URL + `/api/jobs/get-list`;
+      const positionsResponse = await fetch(positionsUrl);
+      const positionsData = await positionsResponse.json();
+      console.log(positionsData);
+      setDatos(positionsData);
       } catch (error) {
+        alert('Error al obtener los datos del candidato o las posiciones.');
       } finally {
         setLoading(false);
       } 
     }
   return (
     <div className="App">
-      <body className="App-header">
+      <header className="App-header">
+        <h1>Challenge Nimble Gravity - Camilo Enriquez</h1>
 
-      </body>
+        <div className="input-container">
+          <input 
+            type="email" 
+            placeholder="Ingrese el correo del postulante" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <button onClick={handleFetch} disabled={loading}>
+            {loading ? 'Cargando...' : 'Obtener posiciones'}
+          </button>
+        </div>
+        {candidate && (
+                <div className="mensaje-bienvenida">
+                  <h2>¡Bienvenido, {candidate.firstName}!</h2>
+                  <p>Ya podés ver las posiciones disponibles abajo.</p>
+                </div>
+        )}
+      </header>
     </div>
   );
 }
