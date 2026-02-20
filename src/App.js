f7515bc7cd6658b0d9c2029a071a7f8797269e42
k1000oEnz <enriquezcamilofranco@gmail.com>
2026-02-20 18:42:23 -0300

    App.js - Armada estructura inicial del fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [candidate, setCandidate] = useState(null);
+  const [datos, setDatos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleFetch = async () => {
+    if(!email) return alert('Por favor, introduzca un correo valido.');
+
+    setLoading(true);
+    try {
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      } 
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-header">
+
+      </body>
     </div>
   );
 }
